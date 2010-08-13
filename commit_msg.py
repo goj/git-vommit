3bbaaef9c2b6734024f09183e210c9e464c6b4e5
Krzysztof Goj <krzysztof.goj@erlang-solutions.com>
2010-08-13 12:27:01 +0200

    Does this work

--- commit_msg.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import urllib2
-from BeautifulSoup import BeautifulSoup
-
-html = urllib2.urlopen('http://whatthecommit.com/').read()
-print BeautifulSoup(html).find(id='content').p.string.strip()
